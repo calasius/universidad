efc8d0fa2cda51216e14f43ebd56346ff2c24892
calasius <calasius@gmail.com>
2020-04-20 01:26:04 -0300

    Agrego ej2 parte 3

--- 2020/tlen/practica1/ej2.hs
+++ 2020/tlen/practica1/ej2.hs
@@ -5,3 +5,6 @@ curry f = \x y -> f(x,y)
 uncurry :: (a -> b -> c) -> ((a,b) -> c)
 
 uncurry f = \(x,y) ->  f x y
+
+-- NO se podria crear la funcion curryN con una cantidad arbitraria de parametros porque necesito saber la cantidad de parametros en 
+-- la definicion de la funcion
