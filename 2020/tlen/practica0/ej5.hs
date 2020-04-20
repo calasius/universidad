9b0bb016648c4d2c9148ce8e6cb2bb53de47419b
calasius <calasius@gmail.com>
2020-04-20 00:26:21 -0300

    Agrego ultimos ejercicios de paractica 0

--- 2020/tlen/practica0/ej5.hs
+++ 2020/tlen/practica0/ej5.hs
@@ -1,7 +1,17 @@
-data AB a = Nil | Bin (AB a) a (AB a)
+data AB a = Nil | Bin (AB a) a (AB a) deriving (Show)
 
 vacioAB :: AB a -> Bool
 
 vacioAB a = case a of 
   Nil -> True
   _ -> False
+
+negacionAB :: AB Bool -> AB Bool
+
+negacionAB Nil = Nil
+negacionAB (Bin i r d) = Bin (negacionAB i) (not r) (negacionAB d)
+
+productoAB :: AB Int -> Int
+
+productoAB Nil = 1
+productoAB (Bin i r d) = r * productoAB(i) * productoAB(d)
